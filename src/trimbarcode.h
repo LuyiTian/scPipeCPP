f083ad85613a3e1c7805e2b8de2948628946cf0f
Luyi Tian <tlytiger@hotmail.com>
2017-05-03 16:11:01 +1000

    Update

--- src/trimbarcode.h
+++ src/trimbarcode.h
@@ -5,9 +5,13 @@
 #include "config_hts.h"
 #include "utils.h"
 
+#ifndef INIT_KSEQ
+#define INIT_KSEQ
+KSEQ_INIT(gzFile, gzread)
+#endif
+
 #ifndef TRIMBARCODE_H
 #define TRIMBARCODE_H
-KSEQ_INIT(gzFile, gzread)
 #define bam1_seq_seti(s, i, c) ( (s)[(i)>>1] = ((s)[(i)>>1] & 0xf<<(((i)&1)<<2)) | (c)<<((~(i)&1)<<2) )
 
 static const char empty_header[] = "@HD\tVN:1.4\tSO:unknown\n";
